f4791ab5f215972863fb29df9b30afd276982511
Victor Ruiz <ruizdvictor@gmail.com>
2017-12-26 06:39:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,41 @@
+var result
+function add(a, b) {
+  result = a + b
+  return result
+}
+
+function subtract(a, b) {
+  result = a - b 
+  return result
+}
+function multiply(a, b) {
+  result = a * b 
+  return result 
+}
+function divide(a, b) {
+  result = a / b 
+  return result
+}
+function inc(n) {
+  n = 0
+  result = n
+  return result
+}
+function dec(n) {
+  n = 0
+  result = n--
+  return result
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
